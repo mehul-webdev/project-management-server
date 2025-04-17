f6df929bd4fe58286f1c1c4acd0087fcb31eb45a
mehulwebdev96@gmail.com <mehulchaudhary330@gmail.com>
2025-04-18 03:17:23 +0530

    updated cors settings

--- src/app.js
+++ src/app.js
@@ -8,14 +8,30 @@ const errorHandler = require("./middlewares/errorHandler");
 const app = express();
 
 const corsOptions = {
-  origin: "https://fullstack-project-management.netlify.app/",
-  credentials: true,
+  origin: (origin, callback) => {
+    // Ensure that the request comes from a trusted source
+    if (
+      !origin ||
+      [
+        "http://localhost:3000",
+        "https://fullstack-project-management.netlify.app",
+      ].includes(origin)
+    ) {
+      callback(null, true);
+    } else {
+      callback(new Error("CORS not allowed"), false);
+    }
+  },
+  credentials: true, // Allow cookies and credentials with the request
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allow only specific HTTP methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Allow the specified headers
 };
 
 // Middlewares
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 // Routes
 app.use("/project-management/v1/todos", todoRoutes);
